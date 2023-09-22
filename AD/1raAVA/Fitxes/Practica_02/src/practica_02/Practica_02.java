60129f46d9f2d813c53ed9363f91e007e772cb6d
andreuet <andsansan5@alu.edu.gva.es>
2023-09-22 18:07:52 +0200

    d

---
+++ AD/1raAVA/Fitxes/Practica_02/src/practica_02/Practica_02.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica_02;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 2DAM_Sanz_Andreu
+ */
+public class Practica_02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<String> moduls = new ArrayList<String>();
+        moduls.add("PSP");
+        moduls.add("SGE");
+        moduls.add("FOL");
+        moduls.add("MDMP");
+        moduls.add("Angles");
+        
+        
+        
+        
+    }
+    
+}
