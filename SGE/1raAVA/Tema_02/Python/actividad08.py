ca637ff63bee337abf33db25def6175590debab6
andreuet <andsansan5@alu.edu.gva.es>
2023-09-27 20:58:32 +0200

    d

--- SGE/1raAVA/Tema_02/Python/actividad08.py
+++ SGE/1raAVA/Tema_02/Python/actividad08.py
@@ -1,21 +1,25 @@
 import random
-max = 100
+max = 99
 min = 0
 intentos = 0
-a = random.randint(min,max)
-print("Bienvenido a mi minijuego, el juego consistra en que debes hadibinar el numero que jo el elegido. El numero esta entre 1-100")
 num=0
+a = 42#random.randint(min,max)
+print("Bienvenido a mi minijuego, el juego consistra en que debes hadibinar el numero que jo el elegido. El numero esta entre 1-99")
 
 
 while num != a :
     try:
-        num = int(input(f"El numero esta entre {min}-{max} que numero crees que es? {a} -"))
+        num = int(input(f"El numero esta entre {int(min)}-{int(max)} que numero crees que es? {a} -"))
         if min > num or max < num:
-            print(f"El numero debe estar entre {min}-{max}")
-        elif a < max/2 and num < max/2:
+            print(f"El numero debe estar entre {int(min)}-{int(max)}")
+        elif a < max/2:
             max/=2
-        elif a > max/2 and num > max/2:
-            min=max/2
+            intentos+=1
+        elif a > max/2:
+            min=(max+min)/2
+            intentos+=1
     except ValueError:
         print("Debes introducir un entero")
-print("Enorabuena, lo has adivinado!!!")
+print(f"""Enorabuena, lo has adivinado!!!
+      Numero de inventos {intentos}
+      """)
