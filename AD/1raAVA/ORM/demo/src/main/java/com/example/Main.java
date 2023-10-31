6989a6714cdf496c714631abbee6fdd20dbf7e7a
andreuet <andsansan5@alu.edu.gva.es>
2023-10-31 19:53:05 +0100

    d

---
+++ AD/1raAVA/ORM/demo/src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import javakarta.annotation.processing.Generated;
+@Entity
+@Table (name = "Main")
+public class Main {
+    
+    String id;
+    String name;
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
