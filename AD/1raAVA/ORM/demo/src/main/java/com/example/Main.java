4bb6af906ad46c5cba1ffd2b50c63caf1d9523fa
andreuet <andsansan5@alu.edu.gva.es>
2023-11-09 21:02:59 +0100

    d

--- AD/1raAVA/ORM/demo/src/main/java/com/example/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-public class Main {
-    
-    String id;
-    String name;
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- AD/1raAVA/ORM/untitled/src/main/java/org/example/Main.java
+++ AD/1raAVA/ORM/untitled/src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 public class Main {
+    @Id
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
