acbe1d375a2e74b3b7e9ca3a727bf2ecf278fa3a
andreuet <andsansan5@alu.edu.gva.es>
2023-11-22 12:00:09 +0100

    d

---
+++ AD/ORM/Primera practica/Practica_05/src/main/java/prueba/Main.java
@@ -0,0 +1,12 @@
+package prueba;
+
+import com.ibm.icu.util.Calendar;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Controller ctr = new Controller();
+        //ctr.fun();
+        System.out.println(1 + "/" + 2 + "/" + 3);
+    }
+}
---
+++ AD/ORM/Primera practica/Preubas/src/main/java/prueba/Main.java
@@ -0,0 +1,8 @@
+package prueba;
+public class Main {
+    
+    public static void main(String[] args) {
+        Controller ctr = new Controller();
+        ctr.fun();
+    }
+}
