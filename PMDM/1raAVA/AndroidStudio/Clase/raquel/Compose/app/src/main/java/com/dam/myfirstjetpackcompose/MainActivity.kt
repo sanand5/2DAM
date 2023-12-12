0ebf7c7844e45ef769cf326062b6b827a98bc35c
andreuet <andsansan5@alu.edu.gva.es>
2023-12-12 17:56:22 +0100

    d

--- PMDM/1raAVA/AndroidStudio/Clase/raquel/Compose/app/src/main/java/com/dam/myfirstjetpackcompose/MainActivity.kt
+++ PMDM/1raAVA/AndroidStudio/Clase/raquel/Compose/app/src/main/java/com/dam/myfirstjetpackcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dam.myfirstjetpackcompose
 import android.annotation.SuppressLint
 import android.hardware.camera2.params.BlackLevelPattern
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,30 +11,41 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -60,8 +72,9 @@ class MainActivity : ComponentActivity() {
                 ) {
                     //Greeting("Android")
                     //App()
-                    LazyColumnExample()
+                    //LazyColumnExample()
                     //SimplesTextField()
+                    BasicScaffold()
                 }
             }
         }
@@ -132,8 +145,12 @@ fun GreetingPreview() {
 //////////// PREVIEW LAZYCOLUMN EXAMPLE //////////
 ////////////////////////////////////////////
 @OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun LazyColumnExample() {
+    var show by rememberSaveable {
+        mutableStateOf(false)
+    }
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
@@ -177,7 +194,20 @@ fun LazyColumnExample() {
                 Text(fontSize = 24.sp, text ="+")
             }
         }
+        item {
+            Button(onClick = { show = true }) {
+                Text(text = "Dialog")
+            }
+        }
     }
+    dialog(
+        show,
+        {show = false},
+        {
+            show = false
+            Log.i("PMDM", "Hola que tal")
+        }
+    )
 }
 
 
@@ -193,5 +223,101 @@ fun SimplesTextField(label: String, ph: String) {
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
         leadingIcon = {Icon(imageVector = Icons.Rounded.AccountCircle, contentDescription = "email")},
         trailingIcon = {Icon(imageVector = Icons.Rounded.AccountCircle, contentDescription = "email")}
+    )
+}
+
+
+@Composable
+fun dialog(show: Boolean, onDissmiss: () -> Unit, onConfirm: () -> Unit): Unit {
+    if (show){
+        AlertDialog(
+            onDismissRequest = { onDissmiss() },
+            confirmButton = {
+                TextButton(onClick = {onConfirm()}) {
+                    Text(text = "Accept")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {onDissmiss()}) {
+                    Text(text = "Dismiss")
+                }
+            },
+            title = { Text(text = "Hola")},
+            text = { Text("Hola, ¿qué tal?") },
+            icon = { Icon(imageVector = Icons.Rounded.AccountCircle, contentDescription = "email")}
+        )
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BasicScaffold(): Unit {
+    Scaffold (
+        topBar = {/*BasicTopBar()*/AdvanceTopBar()},
+        content = {padding -> BasicContent(padding)},
+        floatingActionButton = {BasicFAB()},
+        bottomBar = {BasicBottonBar()}
+    )
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BasicTopBar(): Unit {
+    TopAppBar(title = { Text(
+        text = "PMDM Forum",
+        color = Color.White)},
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary))
+}
+@Composable
+fun BasicContent(padding: PaddingValues): Unit {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)
+    ){
+        Text(text = "Entry 1", fontSize = 20.sp, modifier = Modifier.padding(10.dp))
+        Text(text = "Entry 2", fontSize = 20.sp, modifier = Modifier.padding(10.dp))
+        Text(text = "Entry 3", fontSize = 20.sp, modifier = Modifier.padding(10.dp))
+    }
+}
+@Composable
+fun BasicFAB(): Unit {
+    FloatingActionButton(onClick = { /*TODO*/ }, containerColor = MaterialTheme.colorScheme.primary) {
+        Text(text = "+", fontSize = 24.sp)
+    }
+}
+@Composable
+fun BasicBottonBar(): Unit {
+    BottomAppBar(
+        content = {
+            Text(text = "HOME", fontSize = 20.sp)
+        },
+        containerColor = MaterialTheme.colorScheme.primary
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AdvanceTopBar() {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(
+                content = {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier.size(40.dp)
+                    )
+                },
+                onClick = {},
+
+
+                )
+        },
+        title = {
+            Text(
+                text = "PMDM Forum", color = Color.Cyan
+            )
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary)
     )
 }
