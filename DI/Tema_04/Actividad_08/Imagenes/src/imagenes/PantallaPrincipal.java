cb64b26b2a1182a4f62f366a2b81e87445674921
andreuet <andsansan5@alu.edu.gva.es>
2024-02-07 17:58:33 +0100

    d

--- DI/Tema_04/Actividad_08/Imagenes/src/imagenes/PantallaPrincipal.java
+++ DI/Tema_04/Actividad_08/Imagenes/src/imagenes/PantallaPrincipal.java
@@ -26,7 +26,7 @@ public class PantallaPrincipal extends javax.swing.JFrame {
         ImageIcon imagen = new ImageIcon("src/imagenes/img/linuxwindowsjokes.jpg");
         Icon icono = new ImageIcon(imagen.getImage().getScaledInstance(jLabelImg.getWidth(), jLabelImg.getHeight(), Image.SCALE_DEFAULT ));
         jLabelImg.setIcon(icono);
-        this.setIconImage(new ImageIcon(getClass().getResource("/imagenes/img/OIP.jpg")).getImage());
+        //this.setIconImage(new ImageIcon(getClass().getResource("/imagenes/img/OIP.jpg")).getImage());
     }
 
     /**
